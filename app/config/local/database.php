3959c47bf1060fcdda1cbc70a54b4a36721c24ae
Yorick de Wid <yorick17@outlook.com>
2014-09-09 22:38:43 +0200

    change default install

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -20,23 +20,12 @@ return array(
 
 	'connections' => array(
 
-		'mysql' => array(
-			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'homestead',
-			'username'  => 'homestead',
-			'password'  => 'secret',
-			'charset'   => 'utf8',
-			'collation' => 'utf8_unicode_ci',
-			'prefix'    => '',
-		),
-
 		'pgsql' => array(
 			'driver'   => 'pgsql',
 			'host'     => 'localhost',
-			'database' => 'homestead',
-			'username' => 'homestead',
-			'password' => 'secret',
+			'database' => 'calctool',
+			'username' => 'calctool',
+			'password' => 'q',
 			'charset'  => 'utf8',
 			'prefix'   => '',
 			'schema'   => 'public',
