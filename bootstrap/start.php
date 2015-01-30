8ef9797259b63de15452f4e1d63569eaf201306b
Yorick de Wid <yorick17@outlook.com>
2015-01-30 22:33:36 +0100

    Add hostname to local config

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('webdev', 'don-VirtualBox'),
+	'local' => array('webdev', 'webdev2', 'don-VirtualBox', 'alnx17'),
 	'dev' => array('dev'),
 
 ));
