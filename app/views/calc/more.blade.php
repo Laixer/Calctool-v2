16347f125d432f582aff8bcb1336e7575dd3ca89
yorickdewid <yorick17@outlook.com>
2015-05-02 16:22:59 +0200

    add, delete timesheet from more

--- app/views/calc/more.blade.php
+++ app/views/calc/more.blade.php
@@ -354,6 +354,67 @@ var n = this,
 				});
 			}
 		});
+		$("body").on("blur", ".tsave", function(){
+			var flag = true;
+			var $curThis = $(this);
+			if($curThis.closest("tr").attr("data-id"))
+				return false;
+			$curThis.closest("tr").find("input").each(function(){
+				if(!$(this).val())
+					flag = false;
+			});
+			if(flag){
+				$date = $curThis.closest("tr").find("input[name='date']").val();
+				$hour = $curThis.closest("tr").find("input[name='hour']").val();
+				$type = {{ TimesheetKind::where('kind_name','=','meerwerk')->first()->id }};
+				$activity = $curThis.closest("table").attr("data-id");
+				$note = $curThis.closest("tr").find("input[name='note']").val();
+				$.post("/timesheet/new", {
+					date: $date,
+					hour: $hour,
+					type: $type,
+					activity: $activity,
+					note: $note
+				}, function(data){
+					var json = $.parseJSON(data);
+					$curThis.closest("tr").find("input").removeClass("error-input");
+					if (json.success) {
+						$curThis.closest("tr").attr("data-id", json.id);
+						var rate = {{ $project->hour_rate }};
+						var amount = $curThis.closest("tr").find("input[name='hour']").val().toString().split('.').join('').replace(',', '.');
+						var $curTable = $curThis.closest("table");
+						var json = $.parseJSON(data);
+						$curTable.find("tr:eq(1)").clone().removeAttr("data-id")
+						.find("td:eq(0)").text($date).end()
+						.find("td:eq(1)").text(json.hour).end()
+						.find("td:eq(2)").text('€ '+$.number(rate*amount,2,',','.')).end()
+						.find("td:eq(3)").text($note).end()
+						.prependTo($curTable);
+						$curThis.closest("tr").find("input").val("");
+					} else {
+						$.each(json.message, function(i, item) {
+							if(json.message['name'])
+								$curThis.closest("tr").find("input[name='name']").addClass("error-input");
+							if(json.message['unit'])
+								$curThis.closest("tr").find("input[name='unit']").addClass("error-input");
+							if(json.message['rate'])
+								$curThis.closest("tr").find("input[name='rate']").addClass("error-input");
+							if(json.message['amount'])
+								$curThis.closest("tr").find("input[name='amount']").addClass("error-input");
+						});
+					}
+				}).fail(function(e){
+					console.log(e);
+				});
+			}
+		});
+		$("body").on("click", ".xdeleterow", function(){
+			var $curThis = $(this);
+			if($curThis.closest("tr").attr("data-id"))
+				$.post("/timesheet/delete", {id: $curThis.closest("tr").attr("data-id")}, function(){
+					$curThis.closest("tr").hide("slow");
+				}).fail(function(e) { console.log(e); });
+		});
 		$("body").on("click", ".sdeleterow", function(){
 			var $curThis = $(this);
 			if($curThis.closest("tr").attr("data-id"))
@@ -461,9 +522,7 @@ var n = this,
 													<div class="col-md-4"></div>
 													<div class="col-md-2"><label class="radio-inline"><input data-id="{{ $activity->id }}" class="radio-activity" name="soort{{ $activity->id }}" value="{{ Part::where('part_name','=','contracting')->first()->id }}" type="radio" {{ ( Part::find($activity->part_id)->part_name=='contracting' ? 'checked' : '') }}/>Aanneming</label></div>
 	    											<div class="col-md-2"><label class="radio-inline"><input data-id="{{ $activity->id }}" class="radio-activity" name="soort{{ $activity->id }}" value="{{ Part::where('part_name','=','subcontracting')->first()->id }}" type="radio" {{ ( Part::find($activity->part_id)->part_name=='subcontracting' ? 'checked' : '') }}/>Onderaanneming</label></div>
-													<!--<div class="col-md-2 text-right"><button id="pop-{{$chapter->id.'-'.$activity->id}}" data-container="body" data-toggle="popover" data-placement="bottom" data-content="<textarea></textarea>" data-original-title="A Title" title="" aria-describedby="popover499619" data-id="{{ $activity->id }}" class="btn btn-info btn-xs">Omschrijving toevoegen</button></div>-->
 													<div class="col-md-2 text-right"><button id="pop-{{$chapter->id.'-'.$activity->id}}" data-id="{{ $activity->id }}" data-container="body" data-toggle="popover" data-placement="bottom" data-content="<textarea></textarea>" data-original-title="A Title" title="" aria-describedby="popover499619" class="btn btn-info btn-xs popdesc">Omschrijving toevoegen</button></div>
-
 													<div class="col-md-2 text-right"><button data-id="{{ $activity->id }}" class="btn btn-danger btn-xs deleteact">Verwijderen</button></div>
 												</div>
 												<div class="row">
@@ -471,15 +530,32 @@ var n = this,
 													<div class="col-md-1 text-right"><strong>BTW</strong></div>
 													<div class="col-md-2">
 														<select name="btw" data-id="{{ $activity->id }}" data-type="calc-labor" id="type" class="form-control-sm-text pointer select-tax">
-																@foreach (Tax::all() as $tax)
-																	<option value="{{ $tax->id }}" {{ ($activity->tax_more_labor_id==$tax->id ? 'selected="selected"' : '') }}>{{ $tax->tax_rate }}%</option>
-																@endforeach
+														@foreach (Tax::all() as $tax)
+															<option value="{{ $tax->id }}" {{ ($activity->tax_more_labor_id==$tax->id ? 'selected="selected"' : '') }}>{{ $tax->tax_rate }}%</option>
+														@endforeach
 														</select>
 													</div>
 													<div class="col-md-6"></div>
 												</div>
 												<table class="table table-striped" data-id="{{ $activity->id }}">
-													<?# -- table head -- ?>
+													<?php
+													$count = MoreLabor::where('activity_id','=', $activity->id)->whereNotNull('hour_id')->count('hour_id');
+													if ($count) {
+													?>
+													<thead>
+														<tr>
+															<th class="col-md-1">Datum</th>
+															<th class="col-md-1">Uren</th>
+															<th class="col-md-1">Prijs</th>
+															<th class="col-md-5">Omschrijving</th>
+															<th class="col-md-1">&nbsp;</th>
+															<th class="col-md-1">&nbsp;</th>
+															<th class="col-md-1">&nbsp;</th>
+															<th class="col-md-1">&nbsp;</th>
+															<th class="col-md-1">&nbsp;</th>
+														</tr>
+													</thead>
+													<?php }else { ?>
 													<thead>
 														<tr>
 															<th class="col-md-5">Omschrijving</th>
@@ -492,19 +568,48 @@ var n = this,
 															<th class="col-md-1">&nbsp;</th>
 														</tr>
 													</thead>
+													<?php } ?>
 
-													<?# -- table items -- ?>
 													<tbody>
-														<tr data-id="{{ MoreLabor::where('activity_id','=', $activity->id)->first()['id'] }}"><?# -- item -- ?>
+														<?php
+														if ($count) {
+														?>
+														@foreach (MoreLabor::where('activity_id','=', $activity->id)->whereNotNull('hour_id')->get() as $labor)
+														<tr data-id="{{ $labor->id }}">
+															<td class="col-md-1">{{ Timesheet::find($labor->hour_id)->register_date }}</td>
+															<td class="col-md-1">{{ number_format($labor->amount, 2,",",".") }}</td>
+															<td class="col-md-1"><span class="total-ex-tax">{{ '&euro; '.number_format(MoreRegister::laborTotal($labor->rate, $labor->amount, 2, ",",".")) }}</span></td>
+															<td class="col-md-5">{{ Timesheet::find($labor->hour_id)->note }}</td>
+															<td class="col-md-1">&nbsp;</td>
+															<td class="col-md-1">&nbsp;</td>
+															<td class="col-md-1">&nbsp;</td>
+															<td class="col-md-1 text-right"><button class="btn btn-xs fa btn-danger fa-times xdeleterow"></button></td>
+														</tr>
+														@endforeach
+														<tr>
+															<td class="col-md-1"><input type="date" name="date" id="date" class="form-control-sm-text tsave"/></td>
+															<td class="col-md-1"><input type="number" min="0" name="hour" id="hour" class="form-control-sm-text tsave"/></td>
+															<td class="col-md-1"><span class="total-ex-tax"></span></td>
+															<td class="col-md-5"><input type="text" name="note" id="note" class="form-control-sm-text tsave"/></td>
+															<td class="col-md-1">&nbsp;</td>
+															<td class="col-md-1">&nbsp;</td>
+															<td class="col-md-1">&nbsp;</td>
+															<td class="col-md-1">&nbsp;</td>
+														</tr>
+														<?php }else { ?>
+														@foreach (MoreLabor::where('activity_id','=', $activity->id)->get() as $labor)
+														<tr data-id="{{ $labor->id }}">
 															<td class="col-md-5">Arbeidsuren</td>
 															<td class="col-md-1">&nbsp;</td>
 															<td class="col-md-1">{{ number_format($project->hour_rate_more, 2,",",".") }}</td>
-															<td class="col-md-1"><input data-id="{{ $activity->id }}" name="amount" type="text" value="{{ number_format(MoreLabor::where('activity_id','=', $activity->id)->first()['amount'], 2, ",",".") }}" class="form-control-sm-number labor-amount lsave" /></td>
-															<td class="col-md-1"><span class="total-ex-tax">{{ '&euro; '.number_format(MoreRegister::laborTotal(MoreLabor::where('activity_id','=', $activity->id)->first()['rate'], MoreLabor::where('activity_id','=', $activity->id)->first()['amount'], 2, ",",".")) }}</span></td>
+															<td class="col-md-1"><input data-id="{{ $activity->id }}" name="amount" type="text" value="{{ number_format($labor->amount, 2, ",",".") }}" class="form-control-sm-number labor-amount lsave" /></td>
+															<td class="col-md-1"><span class="total-ex-tax">{{ '&euro; '.number_format(MoreRegister::laborTotal($labor->rate, $labor->amount, 2, ",",".")) }}</span></td>
 															<td class="col-md-1">&nbsp;</td>
 															<td class="col-md-1">&nbsp;</td>
 															<td class="col-md-1 text-right"><button class="btn btn-danger ldeleterow btn-xs fa fa-times"></button></td>
 														</tr>
+														@endforeach
+														<?php } ?>
 													</tbody>
 												</table>
 
