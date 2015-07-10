aa86bce5885b6ff7a9bde87c1580d9f6eba2e3a0
don <d.zandbergen@calctool.nl>
2015-07-10 16:15:43 +0200

    Added columns

--- app/views/user/payment.blade.php
+++ app/views/user/payment.blade.php
@@ -8,43 +8,53 @@
 
 		<div class="col-md-12">
 
-			<h2><strong>Kope kope kope</strong></h2>
+			<h2><strong>Abonnementskeuze</strong></h2>
 			{{ Form::open() }}
 
 			<div class="row">
 				<div class="col-md-1"></div>
-				<div class="col-md-2"><strong>Periode</strong></div>
-				<div class="col-md-2"><strong>Bedrag</strong></div>
+				<div class="col-md-1"><strong>Periode</strong></div>
+				<div class="col-md-1"><strong>Bedrag</strong></div>
+				<div class="col-md-1"><strong>Korting</strong></div>
+				<div class="col-md-2"><strong>Kortingsbedrag</strong></div>
+				<div class="col-md-2"><strong>Te betalen</strong></div>
 			</div>
 			<div class="row">
 				<div class="col-md-1"><input type="radio" name="payoption" value="1" /></div>
-				<div class="col-md-2">Maand</div>
-				<div class="col-md-2">&euro; 29,95</div>
+				<div class="col-md-1">Maand</div>
+				<div class="col-md-1">&euro; 29,95</div>
+				<div class="col-md-1">0%</div>
+				<div class="col-md-2">&euro; 0,00</div>
+				<div class="col-md-2"><strong>&euro; 29,95</strong></div>
 			</div>
 			<div class="row">
 				<div class="col-md-1"><input type="radio" name="payoption" value="4" /></div>
-				<div class="col-md-2">Kwartaal</div>
-				<div class="col-md-2">&euro; 82,95</div>
+				<div class="col-md-1">Kwartaal</div>
+				<div class="col-md-1">&euro; 89,85</div>
+				<div class="col-md-1">10%</div>
+				<div class="col-md-2">&euro; 8,99</div>
+				<div class="col-md-2"><strong>&euro; 80,85</strong></div>
 			</div>
 			<div class="row">
 				<div class="col-md-1"><input type="radio" name="payoption" value="6" /></div>
-				<div class="col-md-2">Half jaar</div>
-				<div class="col-md-2">&euro; 152,95</div>
+				<div class="col-md-1">Half jaar</div>
+				<div class="col-md-1">&euro; 179,70</div>
+				<div class="col-md-1">15%</div>
+				<div class="col-md-2">&euro; 26,95</div>
+				<div class="col-md-2"><strong>&euro; 152,75</strong></div>
 			</div>
 			<div class="row">
 				<div class="col-md-1"><input type="radio" name="payoption" value="12" /></div>
-				<div class="col-md-2">Jaar</div>
-				<div class="col-md-2">&euro; 287,95</div>
+				<div class="col-md-1">Jaar</div>
+				<div class="col-md-1">&euro; 359.40</div>
+				<div class="col-md-1">20%</div>
+				<div class="col-md-2">&euro; 71,89</div>
+				<div class="col-md-2"><strong>&euro; 287,52</strong></div>
 			</div>
-			<div class="row">
-				<div class="col-md-1"><input type="radio" name="payoption" value="13" checked /></div>
-				<div class="col-md-2">Goudse Kaas</div>
-				<div class="col-md-2">&euro; 15.000,95</div>
-			</div><br />
-
+			<br>
 			<div class="row">
 				<div class="col-md-12">
-					<button class="btn btn-primary"><i class="fa fa-check"></i> Koop dan</button>
+					<button class="btn btn-primary"><i class="fa fa-check"></i>Bevestigen</button>
 				</div>
 			</div>
 			{{ Form::close() }}
