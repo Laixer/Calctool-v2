6e9fb7c383daf7be9b45d22d33c07436f591da77
don <d.zandbergen@calctool.nl>
2015-07-15 11:51:47 +0200

    Added active class to wizard

--- app/views/calc/wizard.blade.php
+++ app/views/calc/wizard.blade.php
@@ -1,13 +1,13 @@
 <div class="wizard">
 	<a href="/"> Home</a>
 	<a href="/project-{{ $project->id }}/edit" {{ $page=='project' ? 'class="current"' : '' }} >Project</a>
-	<a href="/calculation/project-{{ $project->id }}">Calculatie</a>
-	<a href="/offer/project-{{ $project->id }}">Offerte</a>
-	<a href="/estimate/project-{{ $project->id }}">Stelpst stelle</a>
-	<a href="/less/project-{{ $project->id }}">Minderwerk</a>
-	<a href="/more/project-{{ $project->id }}">Meerwerk</a>
-	<a href="/invoice/project-{{ $project->id }}">Factuur</a>
-	<a href="/result/project-{{ $project->id }}">Resultaat</a>
+	<a href="/calculation/project-{{ $project->id }}" {{ $page=='calculation' ? 'class="current"' : '' }} >Calculatie</a>
+	<a href="/offer/project-{{ $project->id }}" {{ $page=='offer' ? 'class="current"' : '' }}>Offerte</a>
+	<a href="/estimate/project-{{ $project->id }}" {{ $page=='estimate' ? 'class="current"' : '' }} >Stelpst stelle</a>
+	<a href="/less/project-{{ $project->id }}" {{ $page=='less' ? 'class="current"' : '' }} >Minderwerk</a>
+	<a href="/more/project-{{ $project->id }}" {{ $page=='more' ? 'class="current"' : '' }} >Meerwerk</a>
+	<a href="/invoice/project-{{ $project->id }}" {{ $page=='invoice' ? 'class="current"' : '' }} >Factuur</a>
+	<a href="/result/project-{{ $project->id }}" {{ $page=='result' ? 'class="current"' : '' }} >Resultaat</a>
 </div>
 <hr />
 
