94e585e833934c772f0f5b823be576a486b5d294
don <d.zandbergen@calctool.nl>
2015-07-16 16:11:47 +0200

    not used models deleted and adjusted migrations

--- app/database/migrations/2014_09_11_225236_create_system_table.php
+++ app/database/migrations/2014_09_11_225236_create_system_table.php
@@ -25,20 +25,6 @@ class CreateSystemTable extends Migration {
 			$table->boolean('active')->default('N');
 			$table->nullableTimestamps();
 		});
-
-		Schema::create('system_option', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->string('option_key', 50);
-			$table->text('option_value');
-		});
-
-		Schema::create('system_tooltip', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->string('title', 50);
-			$table->text('tooltip_content');
-		});
 	}
 
 	/**
@@ -48,16 +34,6 @@ class CreateSystemTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::table('system_tooltip', function(Blueprint $table)
-		{
-			Schema::drop('system_tooltip');
-		});
-
-		Schema::table('system_option', function(Blueprint $table)
-		{
-			Schema::drop('system_option');
-		});
-
 		Schema::table('system_message', function(Blueprint $table)
 		{
 			Schema::drop('system_message');
