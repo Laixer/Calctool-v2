43e619b344fc9b212c343185ed15ce9c3ed0dd1c
don <d.zandbergen@calctool.nl>
2015-11-17 15:06:53 +0100

    Demo project

--- database/templates/DemoProjectTemplate.php
+++ database/templates/DemoProjectTemplate.php
@@ -1,5 +1,23 @@
 <?php
 
+use \Calctool\Models\Province;
+use \Calctool\Models\Country;
+use \Calctool\Models\ProjectType;
+use \Calctool\Models\RelationType;
+use \Calctool\Models\RelationKind;
+use \Calctool\Models\ContactFunction;
+use \Calctool\Models\Relation;
+use \Calctool\Models\Project;
+use \Calctool\Models\Contact;
+use \Calctool\Models\Part;
+use \Calctool\Models\PartType;
+use \Calctool\Models\Tax;
+use \Calctool\Models\Chapter;
+use \Calctool\Models\Activity;
+use \Calctool\Models\CalculationLabor;
+use \Calctool\Models\CalculationMaterial;
+use \Calctool\Models\CalculationEquipment;
+
 /*
  * Static Models Only
  * Template for demo project
@@ -33,6 +51,9 @@ class DemoProjectTemplate {
 		$relation->kind_id 			= $relationkind->id;
 		$relation->province_id 		= $province->id;
 		$relation->country_id 		= $country->id;
+		//$iban->iban					='NL45ING0111111111';
+		//$iban->iban_name			='Demo Eigenaar';
+
 
 		$relation->save();
 
@@ -73,14 +94,6 @@ class DemoProjectTemplate {
 
 		$contact->save();
 
-		$iban = new Iban;
-		$iban->iban					='NL45ING0111111111';
-		$iban->iban_name			='Demo Eigenaar';
-		$iban->user_id				= $userid;
-		$iban->relation_id			= $relation->id;
-
-		$iban->save();
-
         $part_contract = Part::where('part_name','=','contracting')->first();
 		$part_type_calc = PartType::where('type_name','=','calculation')->first();
 		$part_subcontract = Part::where('part_name','=','subcontracting')->first();
@@ -89,461 +102,53 @@ class DemoProjectTemplate {
 		$tax2 = Tax::where('tax_rate','=','6')->first();
 		$tax3 = Tax::where('tax_rate','=','0')->first();
 
-		$chapterx = new Chapter;
-		$chapterx->chapter_name = 'Woonkamer';
-		$chapterx->priority = 1;
-		$chapterx->project_id = $project->id;
 
-		$chapterx->save();
 
-		$test_chapter1 = new Chapter;
-		$test_chapter1->chapter_name = 'Woonkamer';
 
 
-       	$test_chapter1 = Chapter::create(array(
-			'chapter_name' => 'Woonkamer',
-			'priority' => 2,
-			'project_id' => $project->new id;
-		));
-
-        //AANNEMING
-       	$test_activity1 = new Activity;
-       	$test_activity1->activity_name = 'Plaatsen van laminaat';
-       	$test_activity1->priority = 1;
-       	$test_activity1->note = 'In de woonkamer wordt kliklaminaat gelegd. De oude vloer wordt door de bewoners verwijdert.';
-       	$test_activity1->chapter_id = $chapterx->id;
-       	$test_activity1->tax_estimate_equipment_id = $tax1->id;
-       	$test_activity1->save();
-
-		 $test_activity1 = Activity::create(array(
-			'activity_name' => 'Plaatsen van laminaat',
-			'priority' => '1',
-			'note' => 'In de woonkamer wordt kliklaminaat gelegd. De oude vloer wordt door de bewoners verwijdert.',
-			'chapter_id' => $chapterx->id,
-			'tax_estimate_equipment_id' => $tax1->id
-		 ));
-
-		 $calculation_labor_activity1 = CalculationLabor::create(array(
-			'rate' => '35.00',
-			'amount' => '10',
-			'isless' => false,
-			'activity_id' => $test_activity1->id
-		 ));
-
-		 $calculation_material_activity1_1 = CalculationMaterial::create(array(
-			'material_name' => 'Ondervloer',
-			'unit' => 'm2',
-			'rate' => '9.50',
-			'amount' => '50',
-			'isless' => false,
-			'activity_id' => $test_activity1->id
-		 ));
-
-		$calculation_material_activity1_2 = CalculationMaterial::create(array(
-			'material_name' => 'Laminaat',
-			'unit' => 'm2',
-			'rate' => '55',
-			'amount' => '50',
-			'isless' => false,
-			'activity_id' => $test_activity1->id
-		 ));
-
-		 $calculation_material_activity1_3 = CalculationMaterial::create(array(
-			'material_name' => 'Plinten',
-			'unit' => 'm1',
-			'rate' => '2.50',
-			'amount' => '90',
-			'isless' => false,
-			'activity_id' => $test_activity1->id
-		 ));
-
-		 //AANNEMING
-		 $test_activity2 = Activity::create(array(
-			'activity_name' => 'Verlagen van het plafond',
-			'priority' => '1',
-			'note' => 'Het bestaande plafond wordt verlaagd met een regelwerk en gipsplaten. Het plafond wordt 10 cm verlaagd. In het nieuwe plafond komen 10 lichtspots. ',
-			'chapter_id' => $test_chapter1->id,
-			'tax_estimate_equipment_id' => $test_tax1->id
-		 ));
-
-		 $calculation_labor_activity2 = CalculationLabor::create(array(
-			'rate' => '35.00',
-			'amount' => '8',
-			'isless' => false,
-			'activity_id' => $test_activity1->id
-		 ));
-
-		 $calculation_material_activity2_1 = CalculationMaterial::create(array(
-			'material_name' => 'Vuren geschaafd 18x69 mm',
-			'unit' => 'm1',
-			'rate' => '0.78',
-			'amount' => '50',
-			'isless' => false,
-			'activity_id' => $test_activity1->id
-		 ));
-
-		$calculation_material_activity2_2 = CalculationMaterial::create(array(
-			'material_name' => 'Vuren geschaafd 44x69 mm',
-			'unit' => 'm1',
-			'rate' => '1.25',
-			'amount' => '120',
-			'isless' => false,
-			'activity_id' => $test_activity1->id
-		 ));
-
-		$calculation_material_activity2_3 = CalculationMaterial::create(array(
-			'material_name' => 'Stucplaten 60x200 cm',
-			'unit' => 'stuk',
-			'rate' => '2.00',
-			'amount' => '40',
-			'isless' => false,
-			'activity_id' => $test_activity1->id
-		 ));
-
-		$calculation_material_activity2_4 = CalculationMaterial::create(array(
-			'material_name' => 'Gipsplaatschroeven',
-			'unit' => 'doos',
-			'rate' => '10.00',
-			'amount' => '1',
-			'isless' => false,
-			'activity_id' => $test_activity1->id
-		 ));
-
-
-
-
-
-
-		//ONDERAANNEMING
-		$test_chapter2 = Chapter::create(array(
-			'chapter_name' => 'Badkamer',
-			'priority' => '1',
-			'project_id' => $project->id
-		 ));
-
-		$test_activity3 = Activity::create(array(
-			'activity_name' => 'Tegelen van de wanden',
-			'priority' => '1',
-			'note' => 'De oude tegels blijven zitten en over deze tegels komen nieuwe tegels.',
-			'chapter_id' => $test_chapter1->id,
-			'tax_estimate_equipment_id' => $test_tax1->id
-		 ));
-
-		 $calculation_labor_activity3 = CalculationLabor::create(array(
-			'rate' => '35.00',
-			'amount' => '16',
-			'isless' => false,
-			'activity_id' => $test_activity1->id
-		 ));
-
-		 $calculation_material_activity3_1 = CalculationMaterial::create(array(
-			 'material_name' => 'Tegels Moza',
-			 'unit' => 'm2',
-			 'rate' => '16.00',
-			 'amount' => '25',
-			 'isless' => false,
-			 'activity_id' => $test_activity1->id
-		 ));
-
-		$calculation_material_activity3_2 = CalculationMaterial::create(array(
-			'material_name' => 'Lijm',
-			'unit' => 'zak',
-			'rate' => '35.00',
-			'amount' => '1',
-			'isless' => false,
-			'activity_id' => $test_activity1->id
-		 ));
-
-		$calculation_material_activity3_3 = CalculationMaterial::create(array(
-			'material_name' => 'Voegsel',
-			'unit' => 'doos',
-			'rate' => '9.50',
-			'amount' => '3',
-			'isless' => false,
-			'activity_id' => $test_activity1->id
-		 ));
-
-		$calculation_material_activity3_4 = CalculationMaterial::create(array(
-			'material_name' => 'Tegelkruisjes',
-			'unit' => 'zak',
-			'rate' => '2',
-			'amount' => '1',
-			'isless' => false,
-			'activity_id' => $test_activity1->id
-		 ));
-
-		$calculation_material_activity3_5 = CalculationMaterial::create(array(
-			'material_name' => 'Primer',
-			'unit' => 'emmer',
-			'rate' => '16',
-			'amount' => '1',
-			'isless' => false,
-			'activity_id' => $test_activity1->id
-		 ));
-
-		//ONDERAANNEMING
-		$test_activity4 = Activity::create(array(
-			'activity_name' => 'Tegelen van de vloeren',
-			'priority' => '1',
-			'note' => 'De oude vloer wordt door de bewoners gesloopt en hierop komt een nieuwe tegelvloer te liggen.',
-			'chapter_id' => $test_chapter1->id,
-			'tax_estimate_equipment_id' => $test_tax1->id
-		 ));
-
-		 $calculation_labor_activity4 = CalculationLabor::create(array(
-			'rate' => '35.00',
-			'amount' => '4',
-			'isless' => false,
-			'activity_id' => $test_activity1->id
-		 ));
-
-		 $calculation_material_activity4_1 = CalculationMaterial::create(array(
-			'material_name' => 'Vloertegels',
-			'unit' => 'm2',
-			'rate' => '45.00',
-			'amount' => '8',
-			'isless' => false,
-			'activity_id' => $test_activity1->id
-		 ));
-
-		$calculation_material_activity4_2 = CalculationMaterial::create(array(
-			'material_name' => 'Voegsel',
-			'unit' => 'doos',
-			'rate' => '9.00',
-			'amount' => '1',
-			'isless' => false,
-			'activity_id' => $test_activity1->id
-		 ));
-
-		$calculation_material_activity4_3 = CalculationMaterial::create(array(
-			'material_name' => 'Sanitairkit',
-			'unit' => 'koker',
-			'rate' => '7.50',
-			'amount' => '5',
-			'isless' => false,
-			'activity_id' => $test_activity1->id
-		 ));
-
-
-
-
-
-
-		$test_chapter3 = Chapter::create(array(
-			'chapter_name' => 'Slaapkamer',
-			'priority' => '1',
-			'project_id' => $project->id
-		 ));
-
-		//ONDERAANNEMING
-		$test_activity5 = Activity::create(array(
-			'activity_name' => 'Sauzen van de wanden',
-			'priority' => '1',
-			'note' => 'De wanden worden opnieuw gesausd in de kleur 9010.',
-			'chapter_id' => $test_chapter1->id,
-			'tax_estimate_equipment_id' => $test_tax1->id
-		 ));
-
-		 $calculation_labor_activity5 = CalculationLabor::create(array(
-			 'rate' => '35.00',
-			 'amount' => '8',
-			 'isless' => false,
-			 'activity_id' => $test_activity1->id
-		 ));
-
-		 $calculation_material_activity5_1 = CalculationMaterial::create(array(
-			 'material_name' => 'Kwasten e.d.',
-			 'unit' => 'post',
-			 'rate' => '15.00',
-			 'amount' => '1',
-			 'isless' => false,
-			 'activity_id' => $test_activity1->id
-		 ));
-
-		$calculation_material_activity5_2 = CalculationMaterial::create(array(
-			'material_name' => 'Saus',
-			'unit' => 'emmer',
-			'rate' => '150.00',
-			'amount' => '4',
-			'isless' => false,
-			'activity_id' => $test_activity1->id
-		 ));
-
-
-
-
-
-		$test_chapter4 = Chapter::create(array(
-			'chapter_name' => 'Zolder',
-			'priority' => '1',
-			'project_id' => $project->id
-		 ));
-
-		//AANNEMING
-		$test_activity6 = Activity::create(array(
-			'activity_name' => 'Isoleren van het dakbeschot',
-			'priority' => '1',
-			'note' => 'Het dakbeschot op zolder wordt geisoleerd met speciale polystyreen isolatieplaten. Hier onder wordt eerst een laag dampdoorlatende folie aangebracht',
-			'chapter_id' => $test_chapter1->id,
-			'tax_estimate_equipment_id' => $test_tax1->id
-		 ));
-
-		 $calculation_labor_activity6 = CalculationLabor::create(array(
-			'rate' => '35.00',
-			'amount' => '16',
-			'isless' => false,
-			'activity_id' => $test_activity1->id
-		 ));
-
-		 $calculation_material_activity6_1 = CalculationMaterial::create(array(
-			 'material_name' => 'Diverse bevestigingsmiddelen',
-			 'unit' => 'post',
-			 'rate' => '100.00',
-			 'amount' => '1',
-			 'isless' => false,
-			 'activity_id' => $test_activity1->id
-		 ));
-
-		$calculation_material_activity6_2 = CalculationMaterial::create(array(
-			'material_name' => 'Folie',
-			'unit' => 'rol',
-			'rate' => '150.00',
-			'amount' => '1',
-			'isless' => false,
-			'activity_id' => $test_activity1->id
-		 ));
-
-		$calculation_material_activity6_3 = CalculationMaterial::create(array(
-			 'material_name' => 'Isolatieplaten',
-			 'unit' => 'm2',
-			 'rate' => '25.00',
-			 'amount' => '50',
-			 'isless' => false,
-			 'activity_id' => $test_activity1->id
-		 ));
-
-		//AANNEMING
-		$test_activity7 = Activity::create(array(
-			'activity_name' => 'Plaatsen van een tussenwand',
-			'priority' => '1',
-			'note' => 'De zolder wordt verdeeld in twee aparte kamers, hiervoor is het nodig om een tussenwand te plaatsen met een deur.',
-			'chapter_id' => $test_chapter1->id,
-			'tax_estimate_equipment_id' => $test_tax1->id
-		 ));
-
-		 $calculation_labor_activity7 = CalculationLabor::create(array(
-			'rate' => '35.00',
-			'amount' => '32',
-			'isless' => false,
-			'activity_id' => $test_activity1->id
-		 ));
-
-		 $calculation_material_activity7_1 = CalculationMaterial::create(array(
-			'material_name' => 'Opdekdeur',
-			'unit' => 'stuk',
-			'rate' => '69.00',
-			'amount' => '1',
-			'isless' => false,
-			'activity_id' => $test_activity1->id
-		 ));
-
-		$calculation_material_activity7_2 = CalculationMaterial::create(array(
-			'material_name' => 'Diverse bevestigingsmiddelen',
-			'unit' => 'post',
-			'rate' => '25.00',
-			'amount' => '1',
-			'isless' => false,
-			'activity_id' => $test_activity1->id
-		 ));
-
-		$calculation_material_activity7_3 = CalculationMaterial::create(array(
-			 'material_name' => 'Gipsplaten',
-			 'unit' => 'stuk',
-			 'rate' => '2.00',
-			 'amount' => '20',
-			 'isless' => false,
-			 'activity_id' => $test_activity1->id
-		 ));
-
-		$calculation_material_activity7_4 = CalculationMaterial::create(array(
-			'material_name' => 'Binnendeurkozijn aluminium',
-			'unit' => 'stuk',
-			'rate' => '149.50',
-			'amount' => '1',
-			'isless' => false,
-			'activity_id' => $test_activity1->id
-		 ));
-
-		$calculation_material_activity7_5 = CalculationMaterial::create(array(
-			 'material_name' => 'Vuren geschaafd 18x69 mm',
-			 'unit' => 'stuk',
-			 'rate' => '0.78',
-			 'amount' => '30',
-			 'isless' => false,
-			 'activity_id' => $test_activity1->id
-		 ));
-
-		$calculation_material_activity7_6 = CalculationMaterial::create(array(
-			'material_name' => 'Vuren geschaafd 44x69 mm',
-			'unit' => 'stuk',
-			'rate' => '1.25',
-			'amount' => '40',
-			'isless' => false,
-			'activity_id' => $test_activity1->id
-		 ));
-
-		$calculation_equipment_activity7_1 = CalculationEquipment::create(array(
-			'equipment_name' => 'Kamersteiger',
-			'unit' => 'stuk',
-			'rate' => '75',
-			'amount' => '1.01',
-			'isless' => false,
-			'activity_id' => $test_activity1->id
-		 ));
-
-
-
-
-		$test_chapter5 = Chapter::create(array(
-			 'chapter_name' => 'Garage',
-			 'priority' => '1',
-			 'project_id' => $project->id
-		 ));
-
-		//ONDERAANNEMING
-		$test_activity8 = Activity::create(array(
-			 'activity_name' => 'Egaliseren van de garagevloer',
-			 'priority' => '1',
-			 'note' => 'De garagevloer is niet egaal en dient geegaliseerd te worden met egaline.',
-			 'chapter_id' => $test_chapter1->id,
-			 'tax_estimate_equipment_id' => $test_tax1->id
-		 ));
-
-		 $calculation_labor_activity8 = CalculationLabor::create(array(
-			'rate' => '35.00',
-			'amount' => '5',
-			'isless' => false,
-			'activity_id' => $test_activity1->id
-		 ));
-
-		 $calculation_material_activity8_1 = CalculationMaterial::create(array(
-			 'material_name' => 'Primer',
-			 'unit' => 'emmer',
-			 'rate' => '25.00',
-			 'amount' => '1',
-			 'isless' => false,
-			 'activity_id' => $test_activity1->id
-		 ));
-
-		$calculation_material_activity8_2 = CalculationMaterial::create(array(
-			'material_name' => 'Egaline',
-			'unit' => 'zak',
-			'rate' => '49.00',
-			'amount' => '4',
-			'isless' => false,
-			'activity_id' => $test_activity1->id
-		));
-
+		$chapter1 = new Chapter;
+		$chapter1->chapter_name = 'Woonkamer';
+		$chapter1->priority = 1;
+		$chapter1->project_id = $project->id;
+		$chapter1->save();
+
+       	$activity1 = new Activity;
+       	$activity1->activity_name = 'Plaatsen van laminaat';
+       	$activity1->priority = 1;
+       	$activity1->note = 'In de woonkamer wordt kliklaminaat gelegd. De oude vloer wordt door de bewoners verwijdert.';
+       	$activity1->chapter_id = $chapter1->id;
+       	$activity1->tax_labor_id = $tax1->id;
+       	$activity1->tax_material_id = $tax1->id;
+       	$activity1->tax_equipment_id = $tax1->id;
+       	$activity1->part_id = 1;
+       	$activity1->part_type_id = 1;
+       	$activity1->save();
+
+
+		$calculation_labor_activity1 = new CalculationLabor;
+		$calculation_labor_activity1->rate = '35.00';
+		$calculation_labor_activity1->amount = '10';
+		$calculation_labor_activity1->isless = false;
+		$calculation_labor_activity1->activity_id = $activity1->id;
+		$calculation_labor_activity1->save();
+
+		$calculation_material_activity1_1 = new CalculationMaterial;
+		$calculation_material_activity1_1->material_name = 'Ondervloer';
+		$calculation_material_activity1_1->unit = 'm2';
+		$calculation_material_activity1_1->rate = '9.50';
+		$calculation_material_activity1_1->amount = '50';
+		$calculation_material_activity1_1->isless = false;
+		$calculation_material_activity1_1->activity_id = $activity1->id;
+		$calculation_material_activity1_1->save();
+		
+		$calculation_equipment_activity1_1 = new CalculationEquipment;
+		$calculation_equipment_activity1_1->equipment_name = 'Ondervloer';
+		$calculation_equipment_activity1_1->unit = 'm2';
+		$calculation_equipment_activity1_1->rate = '9.50';
+		$calculation_equipment_activity1_1->amount = '50';
+		$calculation_equipment_activity1_1->isless = false;
+		$calculation_equipment_activity1_1->activity_id = $activity1->id;
+		$calculation_equipment_activity1_1->save();
      }
   }
 
