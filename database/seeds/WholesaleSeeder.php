5982096e2b489774874c6bcffa0ee361ef7e6cfe
don <d.zandbergen@calctool.nl>
2015-11-17 15:27:20 +0100

    added wholesale

--- database/seeds/WholesaleSeeder.php
+++ database/seeds/WholesaleSeeder.php
@@ -28,28 +28,55 @@ class WholesaleSeeder extends Seeder {
 		//$type_schilder = WholesaleType::where('type_name','=','schilder')->first();
 		//$type_hovenier = WholesaleType::where('type_name','=','hovenier')->first();
 		//$type_sanitair = WholesaleType::where('type_name','=','sanitair')->first();
-		//$type_verhuur = WholesaleType::where('type_name','=','verhuur')->first();
+		$type_verhuur = WholesaleType::where('type_name','=','verhuur')->first();
 		//$type_groothandel = WholesaleType::where('type_name','=','groothandel')->first();
 		//$type_veiling = WholesaleType::where('type_name','=','veiling')->first();
 		//$type_overig = WholesaleType::where('type_name','=','overig')->first();
 
-		/* Test leverancier */
+
 		$wholesale = new Wholesale;
 		$wholesale->company_name = "Bouwmaat NL";
 		$wholesale->type_id = $type_bouw->id;
 		$wholesale->phone = "0332992800";
 		$wholesale->email = "klantenservice@bouwmaat.nl";
 		$wholesale->website = "https://www.bouwmaat.nl";
-		/* Adress */
 		$wholesale->address_street = "Nijverheidsweg";
 		$wholesale->address_number = "17";
 		$wholesale->address_postal = "3751LP";
 		$wholesale->address_city = "Bunschoten";
 		$wholesale->province_id = $province->id;
 		$wholesale->country_id = $country->id;
-
 		$wholesale->save();
+		Supplier::create(array('wholesale_id' => $wholesale->id));
 
+		$wholesale = new Wholesale;
+		$wholesale->company_name = "Stiho Schiedam";
+		$wholesale->type_id = $type_bouw->id;
+		$wholesale->phone = "0104273333";
+		$wholesale->email = "stihoschiedam@stiho.nl";
+		$wholesale->website = "https://www.stiho.nl";
+		$wholesale->address_street = "J. van Galenstraat";
+		$wholesale->address_number = "15";
+		$wholesale->address_postal = "3115JG";
+		$wholesale->address_city = "Shiedam";
+		$wholesale->province_id = $province->id;
+		$wholesale->country_id = $country->id;
+		$wholesale->save();
+		Supplier::create(array('wholesale_id' => $wholesale->id));
+
+		$wholesale = new Wholesale;
+		$wholesale->company_name = "Bo-Rent Schiedam";
+		$wholesale->type_id = $type_verhuur->id;
+		$wholesale->phone = "0786170800";
+		$wholesale->email = "info@bo-rent.nl";
+		$wholesale->website = "https://www.bo-rent.nl";
+		$wholesale->address_street = "Diodeweg";
+		$wholesale->address_number = "12";
+		$wholesale->address_postal = "1627LL";
+		$wholesale->address_city = "Hoorn";
+		$wholesale->province_id = $province->id;
+		$wholesale->country_id = $country->id;
+		$wholesale->save();
 		Supplier::create(array('wholesale_id' => $wholesale->id));
 
 		/*Supplier::create(array('supplier_name' => 'bouwmaat'));
