c41b302cf180a5fa73fb105da2c50582976ee69d
yorickdewid <yorick17@outlook.com>
2015-11-27 21:29:55 +0100

    Clear user content fir

---
+++ app/Console/Commands/StorageClear.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Calctool\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class StorageClear extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'storage:clear';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Remove all user stored content';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $files = scandir('public/user-content/');
+        foreach ($files as $file) {
+            if ($file[0] == ".")
+                continue;
+            unlink('public/user-content/'.$file);
+        }
+    }
+}
