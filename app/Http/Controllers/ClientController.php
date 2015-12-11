b69b7a90f127fdf7fca2550749000ea30892af6f
yorickdewid <yorick17@outlook.com>
2015-12-11 22:24:10 +0100

    Inform user that client post a message

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -4,6 +4,9 @@ namespace Calctool\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use \Calctool\Models\User;
+use \Calctool\Models\MessageBox;
+use \Calctool\Models\Project;
 use \Calctool\Models\ProjectShare;
 
 use \Auth;
@@ -21,15 +24,24 @@ class ClientController extends Controller {
 	{
 
 		$project_share = ProjectShare::where('token', $token)->first();
-		if (! $project_share) {
+		if (!$project_share) {
 			return back();
 		}
+		$project = Project::find($project_share->project_id);
 
 		$project_share->client_note = $request->input('client_note');
 		
 		$project_share->save();
 
-		return back();
+		$message = new MessageBox;
+		$message->subject = 'Opdrachtgever heeft gereageerd';
+		$message->message = 'De opdrachtgever heeft de volgende opmerking geplaatst bij project <a href="/project-' . $project->id . '/edit">' . $project->project_name . '</a>:<br /><br />' . nl2br($request->input('client_note'));
+		$message->from_user = User::where('username', 'system')->first()['id'];
+		$message->user_id =	$project->user_id;
+
+		$message->save();
+
+		return back()->with('success', 'Opmerking toegevoegd aan project');
 	}
 
 }
