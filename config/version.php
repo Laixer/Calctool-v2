c762411dae949348a4dfb2f354111fea13b238f6
yorickdewid <yorick17@outlook.com>
2016-02-06 00:13:45 +0100

    Config change

---
+++ config/version.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Versioning
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the default broadcaster that will be used by the
+    | framework when an event needs to be broadcast. You may set this to
+    | any of the connections defined in the "connections" array below.
+    |
+    */
+
+    'describe' => 'v3.0.8',
+
+];
