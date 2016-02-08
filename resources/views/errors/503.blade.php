3cf0a39e75ee57bd69d5c9ea4e54ca589d0baff9
yorickdewid <yorick17@outlook.com>
2016-02-08 19:37:29 +0100

    Revert composer changes
    - Remove debug data
    - Update 503 screen

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,5 +1,21 @@
 @extends('layout.master')
 
+@section('header')
+<header id="topNav" class="topHead">
+    <div class="container">
+
+        <button class="btn btn-mobile" data-toggle="collapse" data-target=".nav-main-collapse">
+            <i class="fa fa-bars"></i>
+        </button>
+
+        <a class="logo" href="/">
+            <img src="/images/logo2.png" width="229px" alt="Calctool" />
+        </a>
+
+    </div>
+</header>
+@endsection
+
 @section('content')
 <div id="wrapper">
 
@@ -8,9 +24,9 @@
         <div class="row">
 
             <div class="col-md-12 text-center">
-                <div class="e404">503</div>
+                <div class="e404"><i class="fa fa-refresh"></i></div>
                 <h2>
-                    <strong>Moment</strong>, De applicatie wordt bijgewerkt...
+                    De applicatie wordt geüpdatet...
                     <span class="subtitle">Ons excuus, probeer het in een paar minuten nogmaals</span>
                 </h2>
             </div>
