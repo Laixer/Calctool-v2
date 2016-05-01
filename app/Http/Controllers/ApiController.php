6877856bc53cbc9cff5446e1c8d49d7d9210090b
yorickdewid <yorick17@outlook.com>
2016-05-01 16:16:20 +0200

    Add missing class

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -14,6 +14,7 @@ use \Calctool\Models\Wholesale;
 use \Calctool\Models\Relation;
 use \Calctool\Models\RelationKind;
 use \Calctool\Models\Contact;
+use \Calctool\Models\MoreLabor;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller {
