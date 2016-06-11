d67930034c25c032586d6de46622e0234094301d
yorickdewid <yorick17@outlook.com>
2016-06-11 15:14:04 +0200

    Disable debugbar by default

--- config/app.php
+++ config/app.php
@@ -162,7 +162,7 @@ return [
         Intervention\Image\ImageServiceProvider::class,
         Bogardo\Mailgun\MailgunServiceProvider::class,
         Barryvdh\Snappy\ServiceProvider::class,
-        Barryvdh\Debugbar\ServiceProvider::class,
+        // Barryvdh\Debugbar\ServiceProvider::class,
         Nathanmac\Utilities\Parser\ParserServiceProvider::class,
 
     ],
@@ -217,7 +217,7 @@ return [
         'Calctool'  => Calctool\Facades\Calctool::class,
         'Image'     => Intervention\Image\Facades\Image::class,
         'Mailgun'   => Bogardo\Mailgun\Facades\Mailgun::class,
-        'Debugbar' => Barryvdh\Debugbar\Facade::class,
+        // 'Debugbar'  => Barryvdh\Debugbar\Facade::class,
         'PDF'       => Barryvdh\Snappy\Facades\SnappyPdf::class,
         'Parser'    => Nathanmac\Utilities\Parser\Facades\Parser::class,
 
