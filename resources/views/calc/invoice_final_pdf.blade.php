9c702e36132341e3c7e16983de65193bfd96d9e4
DonZandbergen <d.zandbergen@calculatietool.com>
2016-07-16 23:27:19 +0200

    Fixxed row in headers PDF

--- resources/views/calc/invoice_final_pdf.blade.php
+++ resources/views/calc/invoice_final_pdf.blade.php
@@ -100,8 +100,8 @@ $type = ProjectType::find($project->type_id);
                         <div>{{ $relation_self->address_postal . ', ' . $relation_self->address_city }}</div>
                         @if ($relation_self->phone)<div>{{ $relation_self->phone }} </div>@endif  
                         @if ($relation_self->email)<div>{{ $relation_self->email }}</div>@endif 
-                        @if ($relation_self->kvk)<div>{{ $relation_self->kvk }}</div>@endif 
-                        <div>{{ $relation_self->iban }}</div>
+                        @if ($relation_self->kvk)<div>{{ $relation_self->kvk }}&nbsp;</div>@endif 
+                        <div>{{ $relation_self->iban }}&nbsp;</div>
                       </td>
                     </tr>
                   </tbody>
