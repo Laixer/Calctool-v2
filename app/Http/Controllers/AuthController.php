6e0d8db9b20b83b022538bea79f6c0c93719f4c5
yorickdewid <yorick17@outlook.com>
2016-09-16 21:19:56 +0200

    Auto refresh down page

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -324,10 +324,10 @@ class AuthController extends Controller {
 	{
 		$user = User::where('token','=',$token)->where('api','=',$api)->first();
 		if (!$user) {
-			return redirect('/')->withErrors(['error' => ['Activatielink is niet geldig']]);
+			return redirect('login')->withErrors(['activate' => ['Activatielink is niet geldig']]);
 		}
 		if ($user->confirmed_mail) {
-			return redirect('/')->withErrors(['error' => ['Account is al geactiveerd']]);
+			return redirect('login')->withErrors(['activate' => ['Account is al geactiveerd']]);
 		}
 		$user->confirmed_mail = date('Y-m-d H:i:s');
 		$user->save();
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -130,7 +130,7 @@ $(function() {
 
 			<div class="white-row hidden-sm hidden-xs">
 				<h4>Nog Registreren?</h4>			
-				<span>Nog geen account? <a href="/register">Maak er één aan</a>, het is <strong>gratis!</strong></span>
+				<span>Nog geen account? <a href="/register">Maak er &eacute;&eacute;n aan</a>, het is <strong>gratis!</strong></span>
 			</div>
 
 		</section>
