eec3ea8d740e562d46a26ef82bae248318dd5438
yorickdewid <yorick17@outlook.com>
2016-11-02 11:16:32 +0100

    Only projects with offers

--- resources/views/cost/timesheet.blade.php
+++ resources/views/cost/timesheet.blade.php
@@ -8,6 +8,7 @@ use \Calctool\Models\Detail;
 use \Calctool\Models\TimesheetKind;
 use \Calctool\Calculus\TimesheetOverview;
 use \Calctool\Models\MoreLabor;
+use \Calctool\Models\Offer;
 ?>
 
 @extends('layout.master')
@@ -150,7 +151,12 @@ use \Calctool\Models\MoreLabor;
 									<td class="col-md-1"><input type="text" name="hour" id="hour" class="form-control-sm-text" maxlength="4" ng-model="hour"/></td>
 									<td class="col-md-2">
 										<select name="projname" id="projname" class="getact form-control-sm-text" ng-model="projname">
-											@foreach (Project::where('user_id','=',Auth::id())->whereNull('project_close')->get() as $projectname)
+											@foreach (Project::where('user_id',Auth::id())->whereNull('project_close')->get() as $projectname)
+											<?php
+												$offer_last = Offer::where('project_id',$projectname->id)->orderBy('created_at', 'desc')->first();
+												if (!$offer_last || !$offer_last->offer_finish)
+													continue;
+											?>
 											<option data-type="{{ $projectname->type_id }}" value="{{ $projectname->id }}">{{ ucwords($projectname->project_name) }}</option>
 											@endforeach
 										</select>
