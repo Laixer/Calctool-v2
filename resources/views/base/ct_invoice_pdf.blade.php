a3cbce4a265643dd924bf8ed745cd95281da8204
yorickdewid <yorick17@outlook.com>
2017-03-08 20:39:21 +0100

    Use relation profits

--- resources/views/base/ct_invoice_pdf.blade.php
+++ resources/views/base/ct_invoice_pdf.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Termijnfactuur</title>
+    <title>Accountfactuur</title>
     <link rel="stylesheet" href="{{ getcwd() }}/css/pdf.css" media="all" />
   </head>
    <body>
